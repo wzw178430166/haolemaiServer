e1f8aa6bbccfd71ce5143f5357f2aabc3b5eef8c
web_junjie <2632818772@qq.com>
2019-07-26 16:25:37 +0800

    登录和注册

--- haolemaiapps/routes/user.js
+++ haolemaiapps/routes/user.js
@@ -4,10 +4,43 @@ const express=require('express');
 var router=express.Router();
 //引入连接池
 var pool=require('../pool/pool.js');
+//引入手机验证码模块
+const request=require("request");
+const querystring=require("querystring");
+var code=("000000"+Math.floor(Math.random()*999999)).slice(-6)
+// parse application/json
+// router.get("/liang",(req,res)=>{
+//     res.send("hello world");
+//     console.log("hello world")
+// });
+router.post("/sms_send",(req,res)=>{
+    var captchaCondition=(Object.keys(req.body)[0]);
+    console.log(captchaCondition);
+    var phone=captchaCondition.phone;
+    console.log(phone)
+    var queryData = querystring.stringify({
+        "mobile": req.body.phone,  // 接受短信的用户手机号码
+        "tpl_id": req.body.tpl_id,  // 您申请的短信模板ID，根据实际情况修改
+        "tpl_value": `#code#=${code}`,  // 您设置的模板变量，根据实际情况修改
+        "key": req.body.key,  // 应用APPKEY(应用详细页查询)
+    });
+    
+    var queryUrl = 'http://v.juhe.cn/sms/send?'+queryData;
+    
+    request(queryUrl, function (error, response, body, tpl_value) {
+        if (!error && response.statusCode == 200) {
+            console.log(body) // 打印接口返回内容
+            
+            // var jsonObj = JSON.parse(body); // 解析接口返回的JSON内容
+            res.send(tpl_value);
+            return;
+        } 
+    })
+});
 //引入session模块
  //配置session
  //添加路由（用户注册）
- router.post('/add',function(req,res){
+ router.post('/register',function(req,res){
 	 var obj1=req.body;
 	 console.log(obj1.phone);
 	 pool.query('SELECT count(*) as counta FROM users WHERE phone=?',[obj1.phone],function(err,result){
@@ -19,7 +52,28 @@ var pool=require('../pool/pool.js');
         }
 	});
  });
-
+ router.post('/add',function(req,res){
+    var phone = req.body.phone;
+    // var upwd = req.body.upwd;
+    // console.log(phone);
+    var sql1 = "SELECT count(*) as counta FROM users WHERE phone=? ";
+    pool.query(sql1, phone, function (err, result) {
+        if (err) throw err;
+        if (result[0].counta > 0) {
+            res.send({code:-1,msg:"该手机号已注册"});
+        } else {
+        var sql2="INSERT INTO users(phone)VALUES(?)";
+        pool.query(sql2,[phone],(err,result)=>{
+            if(err)throw err;
+            if(result.affectedRow==0){
+                res.send("1");
+            }else{
+                res.send("0");
+            }
+        })
+        }
+    });
+});
 //用户登录 路由
 router.get('/login',function(req,res){
     //  req.on('data',function(bnf){
@@ -75,7 +129,7 @@ router.get("/cart",(req,res)=>{
     //3:json
   })
 
-  
+
 //导出用户路由器对象   /user
 module.exports=router;
 
